c942d49aa679ca9d96268af98a232f4911288baf
SHANTANU KUMAR <79790478+shantanu400@users.noreply.github.com>
2024-06-27 12:51:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ class Person{
 		this.age=age;
 	}
 	greet(){
-		console.log("Hello, my name is ${name}, I am <age> years old.");
+		console.log(`Hello, my name is ${name}, I am <age> years old.`);
 	}
 	
 }
@@ -14,16 +14,14 @@ class Person{
 class Employee extends Person{
 	constructor(name,age,jobTitle){
 		super(name,age);
-		
+		this.jobTitle=jobTitle;
 	}
-	jobGreet{
-		console.log("Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.")
+	jobGreet(){
+	
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	}
 }
 
-function Person(name, age) {}
-
-function Employee(name, age, jobTitle) {}
 
 
 
